3a924dafac498f0dc2d27b55e14ceaee31894185
vishnugaddime <vishnugaddime00@gmail.com>
2022-02-11 13:41:52 +0530

    commit

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 print("hello this is shubham")
 print("jaldiii krro reee bhaii")
-<<<<<<< HEAD
-print("Hello this is Vibhav")
-print("lol")
-print("coding full speeeeed")
-print("kya chl rha hai?")
-print("jai mata di")
-=======
 
 print("Hello this is Vibhav")
 
@@ -15,5 +8,4 @@ print("coding full speeeeed")
 print("kya chl rha hai?")
 
 print("Hii this abhijit")
-print("Perfect Guys, Lets Go")
->>>>>>> 8c5e3bc4bda5f18ed599040401998ae70cf28779
+print("hi this is Vishnu")
